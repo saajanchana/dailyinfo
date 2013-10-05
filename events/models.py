0ef69f86521ff468f37454629b73340902540633
saajanchana <saajan.chana@gmail.com>
2013-10-05 16:18:28 +0100

    Rewrite of description shortener

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import safestring
+import django.core.urlresolvers as urlresolvers
 from misc import date2str, time2str
 from datetime import datetime, date, time
 from bs4 import BeautifulSoup, NavigableString
@@ -63,40 +64,81 @@ class Event(models.Model):
         return safestring.mark_safe(self.description)
 
     def word_count(self, text):
-        words = re.split(r'[\s]*', text)
+        words = re.split(r'[\s]+', text)
         return len(words)
 
-    def get_tag_contents(self, tag, max_words):
+    def keep_first_nwords(self, tag, max_words):
+        truncated = False
+        if self.word_count(tag) > max_words:
+            if isinstance(tag, NavigableString):
+                word_list = re.split(r'[\s]+', tag)
+                tag.replace(tag.new_string(" ".join(word_list)))
+
+            else:
+                words = 0
+                for child in tag.children:
+                    if words + self.word_count(child) <= max_words:
+                        words += self.word_count(child)
+                    elif words >= max_words:
+                        tag.decompose()
+                    else:
+                        keep_first_nwords(child, max_words - nwords)
+
+            truncated = True
+        return truncated
+
+    def xxxx(self):
+        print 'get_tag_contents(%s, %d)' % (tag, max_words)
         if isinstance(tag, NavigableString):
             if self.word_count(tag) <= max_words:
+                print 'NavigableString: returned whole tag'
                 return (tag, self.word_count(tag))
             else:
                 words = re.split(r'[\s]*', tag)
+                print 'NavigableString: returned first %d words out of %d' % (max_words, self.word_count(tag))
                 return (" ".join(words[:max_words]), max_words)
 
         elif self.word_count(tag.get_text()) <= max_words:
+            print 'returned entire tag (%d words)' % self.word_count(tag.get_text())
             return (tag, self.word_count(tag.get_text()))
 
         else:
-            new_tag = BeautifulSoup().new_tag(tag.name, **tag.attrs)
-            for child in tag.children:
-                (st, nwords) = self.get_tag_contents(child, max_words)
+            if tag.name != '[document]':
+                new_tag = BeautifulSoup().new_tag(tag.name, **tag.attrs)
+            else:
+                new_tag = BeautifulSoup()
+            nwords = 0
+            contents = tag.contents
+            for child in contents: print child
+
+            for child in contents:
+                print "Parsing child %s" % child
+                (st, tag_words) = self.get_tag_contents(child, max_words)
                 new_tag.append(st)
-                max_words -= nwords
-                if max_words == 0:
+                nwords += tag_words
+                if nwords == max_words:
+                    print 'Done'
                     break
-            return (new_tag, max_words)
+
+            print 'returned %s' % new_tag
+            return (new_tag, nwords)
 
     def description_short(self):
         # get up to 20 words
-        soup_full = BeautifulSoup(self.description)
-        (soup_short, nwords) = self.get_tag_contents(soup_full, 20)
+        soup = BeautifulSoup(self.description)
+        nwords = self.keep_first_nwords(soup, 50)
+
+        if nwords < self.word_count(soup.get_text()):
+            more_info_link = soup.new_tag('a', href=urlresolvers.reverse('event', kwargs = {'pk', str(self.id)}))
+            more_info_link['class'] = 'more_info_link'
+            more_info_link.append('[more info]')
+            soup.append(more_info_link)
 
         # get rid of paragraphs
-        for p_tag in soup_short.findAll('p'):
+        for p_tag in soup.findAll('p'):
             p_tag.unwrap()
 
-        return safestring.mark_safe(soup_short.decode(formatter='html'))
+        return safestring.mark_safe(soup.decode(formatter='html'))
 
     @staticmethod
     def sanitise_html(text, is_html):
